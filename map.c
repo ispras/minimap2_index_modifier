0a1a87598da5e9d482b54e8e0eddf547f39ccff6
EgorGuga <guguchkin@ispras.ru>
2025-10-06 16:11:45 +0300

    just call mm_set_parent second time

--- map.c
+++ map.c
@@ -545,31 +545,8 @@ void mm_map_frag_core(const mm_idx_t *mi, int n_segs, const int *qlens, const ch
 					regs[i] = remove_second_suboptimal_alignment(regs[i], &n_regs[i], z, opt->a * 2 + opt->b);
 				}
 			}
-
+   
 			mm_set_parent(b->km, opt->mask_level, opt->mask_len, n_regs[i], regs[i], opt->a * 2 + opt->b, opt->flag&MM_F_HARD_MLEVEL, opt->alt_drop); // update mm_reg1_t::parent
-   			
-			int max_dpmax2 = 0;
-			int max_score = 0;
-			for (z = 1; z < n_regs[i]; z++) {
-				regs[i][z].id = z;
-				if (regs[i][z].p->dp_score > max_dpmax2) {
-					max_dpmax2 = regs[i][z].p->dp_score;
-				}
-				if (regs[i][z].score > max_score) {
-					max_score = regs[i][z].score;
-				}
-			}
-
-			if (n_regs[i] > 0) {
-				if (regs[i][0].p != NULL)
-					regs[i][0].p->dp_max2 = max_dpmax2;
-
-				//mm_reg1_t *copy = (mm_reg1_t *)malloc(n_regs[i] * sizeof(mm_reg1_t));
-				//memcpy(copy, regs[i], n_regs[i] * sizeof(mm_reg1_t));
-   				mm_set_parent(b->km, opt->mask_level, opt->mask_len, n_regs[i], regs[i], opt->a * 2 + opt->b, opt->flag&MM_F_HARD_MLEVEL, opt->alt_drop);
-
-   				//free(copy);
-			}
 
 			mm_set_mapq2(b->km, n_regs[i], regs[i], opt->min_chain_score, opt->a, rep_len, is_sr || is_sr_rna, is_splice, chrs_to_drop);
 			free_chromosome_names(chrs_to_drop, chrs_to_drop_count);
